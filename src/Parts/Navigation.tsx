6d1354974c9a1ec3b7595dbcb75fd9e0dd286f90
Unluckymichell <39999330+Unluckymichell@users.noreply.github.com>
2025-06-05 14:55:34 +0200

    Add react-confetti-explosion dependency and enhance Countdown component with confetti effect on countdown completion

--- src/Parts/Navigation.tsx
+++ src/Parts/Navigation.tsx
@@ -16,6 +16,8 @@ interface NavigationProps extends React.PropsWithChildren {
 export const IntersectionObserverContext =
   createContext<IntersectionObserver | null>(null);
 
+export const SectionsInViewContext = createContext<string[]>([]);
+
 function Navigation({ links, children }: NavigationProps) {
   const ulRef = useRef<HTMLUListElement>(null);
   const [showButtons, setShowButtons] = useState(false);
@@ -24,8 +26,12 @@ function Navigation({ links, children }: NavigationProps) {
   const inViewNavRef = useRef<HTMLAnchorElement>(null);
   const scrollToPagePos = () => {
     console.log("scrollend", inViewNavRef.current);
-    inViewNavRef.current?.scrollIntoView({ behavior: "smooth", block: 'nearest', inline: 'center' });
-  }
+    inViewNavRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  };
 
   const observerhandler = useRef<IntersectionObserverCallback>(null);
   const makeObserver = () =>
@@ -76,7 +82,7 @@ function Navigation({ links, children }: NavigationProps) {
   const scrollNavlinks = (dir: -1 | 1) => {
     ulRef.current?.scrollBy({
       behavior: "smooth",
-      left: dir * 100
+      left: dir * 100,
     });
   };
 
@@ -92,76 +98,84 @@ function Navigation({ links, children }: NavigationProps) {
   let isFirst = true;
   return (
     <IntersectionObserverContext.Provider value={observer}>
-      <nav
-        className={classNames(
-          "w-full h-18 sm:h-12 sticky top-0 z-1",
-          "flex flex-col justify-between items-stretch",
-          "sm:flex-row sm:items-stretch",
-          "bg-[var(--highlight)] shadow-[0px_5px_5px_rgba(0,_0,_0,_0.2)]"
-        )}
-      >
-        <Link
-          className="w-max px-4 mx-auto sm:mx-0 flex items-center"
-          href={"/"}
+      <SectionsInViewContext.Provider value={sectionInView}>
+        <nav
+          className={classNames(
+            "w-full h-18 sm:h-12 sticky top-0 z-1",
+            "flex flex-col justify-between items-stretch",
+            "sm:flex-row sm:items-stretch",
+            "bg-[var(--highlight)] shadow-[0px_5px_5px_rgba(0,_0,_0,_0.2)]"
+          )}
         >
-          <span className="text-border-1 hover:scale-110 transition-[scale] text-lg sm:text-xl text-blue-100 w-max font-extrabold select-none font-(family-name:--font-lobster)">
-            Corinna & Michael
-          </span>
-        </Link>
-        <div className="flex items-stretch grow-1 min-w-0 overflow-clip">
-          <button
-            className={classNames("text-gray-700 shadow-[4px_0_5px_rgba(0,_0,_0,_0.4)]", {
-              hidden: !showButtons,
-            })}
-            onClick={() => scrollNavlinks(-1)}
+          <Link
+            className="w-max px-4 mx-auto sm:mx-0 flex items-center"
+            href={"/"}
           >
-            <FaCaretLeft className="text-2xl my-auto mx-2" />
-          </button>
-          <ul
-            ref={ulRef}
-            className="min-w-0 flex-1 flex items-stretch overflow-x-scroll overflow-y-clip no-scrollbar"
-          >
-            {links.map((nav) => (
-              <li
-                key={nav.link}
-                className={classNames(
-                  "flex items-center px-4 text-nowrap transition-all box-border",
-                  nav.id && sectionInView.includes(nav.id)
-                    ? isFirst
-                      ? "border-b-4 border-gray-700"
-                      : "border-b-4 border-gray-500/50"
-                    : "border-b-0 border-gray-500/50",
-                  "hover:border-b-8 hover:underline focus-within:border-b-8"
-                )}
-              >
-                <Link
-                  href={nav.link}
-                  className="text-gray-700 font-extrabold select-none hover:scale-110"
-                  ref={
-                    nav.id &&
-                    sectionInView.includes(nav.id) &&
-                    isFirst &&
-                    !(isFirst = false)
-                      ? inViewNavRef
-                      : undefined
-                  }
+            <span className="text-border-1 hover:scale-110 transition-[scale] text-lg sm:text-xl text-blue-100 w-max font-extrabold select-none font-(family-name:--font-lobster)">
+              Corinna & Michael
+            </span>
+          </Link>
+          <div className="flex items-stretch grow-1 min-w-0 overflow-clip">
+            <button
+              className={classNames(
+                "text-gray-700 shadow-[4px_0_5px_rgba(0,_0,_0,_0.4)]",
+                {
+                  hidden: !showButtons,
+                }
+              )}
+              onClick={() => scrollNavlinks(-1)}
+            >
+              <FaCaretLeft className="text-2xl my-auto mx-2" />
+            </button>
+            <ul
+              ref={ulRef}
+              className="min-w-0 flex-1 flex items-stretch overflow-x-scroll overflow-y-clip no-scrollbar"
+            >
+              {links.map((nav) => (
+                <li
+                  key={nav.link}
+                  className={classNames(
+                    "flex items-center px-4 text-nowrap transition-all box-border",
+                    nav.id && sectionInView.includes(nav.id)
+                      ? isFirst
+                        ? "border-b-4 border-gray-700"
+                        : "border-b-4 border-gray-500/50"
+                      : "border-b-0 border-gray-500/50",
+                    "hover:border-b-8 hover:underline focus-within:border-b-8"
+                  )}
                 >
-                  {nav.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <button
-            className={classNames("text-gray-700 shadow-[-4px_0_5px_rgba(0,_0,_0,_0.4)]", {
-              hidden: !showButtons,
-            })}
-            onClick={() => scrollNavlinks(1)}
-          >
-            <FaCaretRight className="text-2xl my-auto mx-2" />
-          </button>
-        </div>
-      </nav>
-      {children}
+                  <Link
+                    href={nav.link}
+                    className="text-gray-700 font-extrabold select-none hover:scale-110"
+                    ref={
+                      nav.id &&
+                      sectionInView.includes(nav.id) &&
+                      isFirst &&
+                      !(isFirst = false)
+                        ? inViewNavRef
+                        : undefined
+                    }
+                  >
+                    {nav.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <button
+              className={classNames(
+                "text-gray-700 shadow-[-4px_0_5px_rgba(0,_0,_0,_0.4)]",
+                {
+                  hidden: !showButtons,
+                }
+              )}
+              onClick={() => scrollNavlinks(1)}
+            >
+              <FaCaretRight className="text-2xl my-auto mx-2" />
+            </button>
+          </div>
+        </nav>
+        {children}
+      </SectionsInViewContext.Provider>
     </IntersectionObserverContext.Provider>
   );
 }
